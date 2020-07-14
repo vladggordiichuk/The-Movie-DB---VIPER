2b242878d37bc3b0cbd909c583ef972c2759fc1b
Vlad Gordiichuk <vladggordiychuk@gmail.com>
2020-07-14 15:10:03 +0300

    Initial Commit

---
+++ The Movie DB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Movie DB
+//
+//  Created by Vlad Gordiichuk on 14.07.2020.
+//  Copyright © 2020 Vlad Gordiichuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
